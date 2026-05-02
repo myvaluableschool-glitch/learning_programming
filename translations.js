cc9c46898cffb28b53ec2be5d1d68be3df3004c5
myvaluableschool-glitch <myvaluableschool@gmail.com>
2026-05-02 18:27:51 +0100

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,28 @@
+const translations = {
+  fr: {
+    hero_title: "Apprends la programmation en C en 30 jours",
+    hero_desc: "Une formation 100% pratique pour devenir autonome rapidement",
+    formations: "Nos niveaux de formation",
+    debutant: "Niveau Débutant",
+    intermediaire: "Niveau Intermédiaire",
+    expert: "Niveau Expert"
+  },
+
+  en: {
+    hero_title: "Learn C programming in 30 days",
+    hero_desc: "A 100% practical course to become autonomous quickly",
+    formations: "Our training levels",
+    debutant: "Beginner Level",
+    intermediaire: "Intermediate Level",
+    expert: "Expert Level"
+  },
+
+  ar: {
+    hero_title: "تعلم البرمجة بلغة C في 30 يومًا",
+    hero_desc: "دورة عملية 100٪ لتصبح مستقلاً بسرعة",
+    formations: "مستويات التكوين",
+    debutant: "مستوى مبتدئ",
+    intermediaire: "مستوى متوسط",
+    expert: "مستوى متقدم"
+  }
+};
